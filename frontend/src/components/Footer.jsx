60e1140a83c1b4880e9e4198a08b5bf6930dc8fc
gsing352 <gsing352@uwo.ca>
2023-11-22 18:44:26 -0500

    Updated the footer of the react app

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,126 +2,63 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="text-center text-lg-start bg-body-tertiary text-muted">
-      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-        <div className="me-5 d-none d-lg-block">
-          <span>Get connected with us on social networks:</span>
-        </div>
-
-        <div>
-          <a href="" className="me-4 text-reset">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-          <a href="" className="me-4 text-reset">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="" className="me-4 text-reset">
-            <i className="fab fa-google"></i>
-          </a>
-          <a href="" className="me-4 text-reset">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="" className="me-4 text-reset">
-            <i className="fab fa-linkedin"></i>
-          </a>
-          <a href="" className="me-4 text-reset">
-            <i className="fab fa-github"></i>
-          </a>
-        </div>
-      </section>
-
-      <section className="">
-        <div className="container text-center text-md-start mt-5">
-          <div className="row mt-3">
-            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">
-                <i className="fas fa-gem me-3"></i>Company name
-              </h6>
-              <p>
-                Here you can use rows and columns to organize your footer
-                content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit.
-              </p>
-            </div>
-
-            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
-              <p>
-                <a href="#!" className="text-reset">
-                  Angular
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  React
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Vue
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Laravel
-                </a>
-              </p>
-            </div>
-
-            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
-              <p>
-                <a href="#!" className="text-reset">
-                  Pricing
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Settings
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Orders
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-reset">
-                  Help
-                </a>
-              </p>
-            </div>
-
-            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-              <p>
-                <i className="fas fa-home me-3"></i> New York, NY 10012, US
-              </p>
-              <p>
-                <i className="fas fa-envelope me-3"></i>
-                info@example.com
-              </p>
-              <p>
-                <i className="fas fa-phone me-3"></i> + 01 234 567 88
-              </p>
-              <p>
-                <i className="fas fa-print me-3"></i> + 01 234 567 89
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <div
-        className="text-center p-4"
-        style="background-color: rgba(0, 0, 0, 0.05);"
-      >
-        © 2021 Copyright:
-        <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
-          MDBootstrap.com
-        </a>
+    <footer className="py-5">
+    <div className="row">
+      <div className="col-2">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+        </ul>
       </div>
-    </footer>
+
+      <div className="col-2">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+        </ul>
+      </div>
+
+      <div className="col-2">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+        </ul>
+      </div>
+
+      <div className="col-4 offset-1">
+        <form>
+          <h5>Subscribe to our newsletter</h5>
+          <p>Monthly digest of whats new and exciting from us.</p>
+          <div className="d-flex w-100 gap-2">
+            <label for="newsletter1" className="visually-hidden">Email address</label>
+            <input id="newsletter1" type="text" className="form-control" placeholder="Email address"/>
+            <button className="btn btn-primary" type="button">Subscribe</button>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    <div className="d-flex justify-content-between py-4 my-4 border-top">
+      <p>© 2021 Company, Inc. All rights reserved.</p>
+      <ul className="list-unstyled d-flex">
+        <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+        <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+        <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+      </ul>
+    </div>
+  </footer>
   );
 };
 
