7874e34ad722abcdc94e7771ffdc9df450581f85
gsing352 <gsing352@uwo.ca>
2023-11-23 18:38:38 -0500

    Added the filtering logic to filter books

--- frontend/src/components/BookItem.jsx
+++ frontend/src/components/BookItem.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const BookItem = ({ book, onViewDetails, onAddToCart }) => {
-    return (
-        <div className="book-item">
-            <img src={book.volumeInfo.imageLinks?.thumbnail} alt={book.volumeInfo.title} />
-            <h3>{book.volumeInfo.title}</h3>
-            {book.volumeInfo.authors && <p>Authors: {book.volumeInfo.authors.join(', ')}</p>}
-            {book.volumeInfo.publishedDate && <p>Published Date: {book.volumeInfo.publishedDate}</p>}
+  return (
+    <div className="book-item">
+      <img
+        src={book.volumeInfo.imageLinks?.thumbnail}
+        alt={book.volumeInfo.title}
+      />
+      <h3>{book.volumeInfo.title}</h3>
+      {book.volumeInfo.authors && (
+        <p>Authors: {book.volumeInfo.authors.join(", ")}</p>
+      )}
+      {book.volumeInfo.publishedDate && (
+        <p>Published Date: {book.volumeInfo.publishedDate}</p>
+      )}
 
-            <button onClick={() => onViewDetails(book)}>View Details</button>
-            <button onClick={() => onAddToCart(book)}>Add to Cart</button>
-        </div>
-    );
+      <button onClick={() => onViewDetails(book)}>View Details</button>
+      <button onClick={() => onAddToCart(book)}>Add to Cart</button>
+    </div>
+  );
 };
 
 export default BookItem;
--- frontend/src/pages/BooksList.jsx
+++ frontend/src/pages/BooksList.jsx
@@ -1,27 +1,38 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import BookItem from "../components/BookItem"; // Assuming you have a BookItem component as described earlier
+import BookItem from "../components/BookItem";
+import BookFilter from "../components/BookFilter";
+import './BooksList.css';
+
 
 const BooksList = () => {
   const [books, setBooks] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [query, setQuery] = useState("science");
 
-  useEffect(() => {
-    if (searchTerm) {
-      fetchBooks();
-    }
-  }, [searchTerm]);
-
-  const fetchBooks = async () => {
+  const fetchBooks = async (query) => {
     try {
       const response = await axios.get(
-        `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=AIzaSyB48Lo0vxO_yQqHUshxjpTsiRqn4QUZCNw`
+        `https://www.googleapis.com/books/v1/volumes?q=${query}&key=AIzaSyB48Lo0vxO_yQqHUshxjpTsiRqn4QUZCNw`
       );
       setBooks(response.data.items || []);
     } catch (error) {
-      console.error("Error fetching data from Google Books API:", error);
+      console.error("Error fetching data:", error);
     }
   };
+  const handleFilterChange = (filter) => {
+    // Construct your query based on the filters
+    let query = [];
+    if (filter.title) query.push(`intitle:${filter.title}`);
+    if (filter.author) query.push(`inauthor:${filter.author}`);
+    if (filter.genre) query.push(`subject:${filter.genre}`);
+    if (filter.language) query.push(`langRestrict:${filter.language}`);
+
+    fetchBooks(query.join("+"));
+  };
+
+  useEffect(() => {
+    fetchBooks("happy-living"); // Fetch initial set of books or provide a default query
+  }, []);
 
   const handleViewDetails = (book) => {
     // Placeholder for view details logic
@@ -34,23 +45,22 @@ const BooksList = () => {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Search for books"
-      />
-      <button onClick={fetchBooks}>Search</button>
-      <div className="books-grid">
-        {books.map((book, index) => (
-          <BookItem
-            key={book.id} // It's better to use a unique identifier like book.id
-            book={book}
-            onViewDetails={() => handleViewDetails(book)}
-            onAddToCart={() => handleAddToCart(book)}
-          />
-        ))}
+    <div className="books-list-container">
+      <div className="filter-column">
+        <BookFilter onFilterChange={handleFilterChange} />
+      </div>
+
+      <div className="books-column">
+        <div className="books-grid">
+          {books.map((book, index) => (
+            <BookItem
+              key={book.id} // It's better to use a unique identifier like book.id
+              book={book}
+              onViewDetails={() => handleViewDetails(book)}
+              onAddToCart={() => handleAddToCart(book)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
