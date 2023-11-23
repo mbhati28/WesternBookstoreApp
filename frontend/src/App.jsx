f7b9c586000cd9e1e30e448cdc710fc579bdd6c7
gsing352 <gsing352@uwo.ca>
2023-11-23 16:07:07 -0500

    Added the routes using React router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
-function App() {
-  
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import Footer from "./components/Footer";
 
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
   return (
-    <h1>hello world</h1>
-  )
+    <Router>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+      </Routes>
+      <Footer></Footer>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <a className="navbar-brand" href="">
+        Navbar
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-      <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
-      <a className="nav-item nav-link" href="#">Features</a>
-      <a className="nav-item nav-link" href="#">Pricing</a>
-      <a className="nav-item nav-link disabled" href="#">Disabled</a>
-      </div>
+        <div className="navbar-nav">
+          <Link className="nav-item nav-link" to="/">
+            Home
+          </Link>
+          <Link className="nav-item nav-link" to="/books">
+            Browse Books
+          </Link>
+          <Link className="nav-item nav-link" to="/cart">
+            Cart
+          </Link>
+          <Link className="nav-item nav-link" to="/order">
+            Order
+          </Link>
+          <Link className="nav-item nav-link" to="/login">
+            Login
+          </Link>
+          <Link className="nav-item nav-link" to="/signup">
+            Sign Up
+          </Link>
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
