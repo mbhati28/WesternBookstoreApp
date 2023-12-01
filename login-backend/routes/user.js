fb148a7440cfb633871fba74017e48c5de4200d1
gaganuwo <gsing363@uwo.ca>
2023-11-30 20:40:53 -0500

    retrieve user order data

--- login-backend/routes/user.js
+++ login-backend/routes/user.js
@@ -66,5 +66,14 @@ router.post('/order/:userId', async (req, res) => {
     }
 });
 
+// Get all orders for a user
+router.get('/orders/:userId', async (req, res) => {
+    try {
+        const orders = await Order.find({ userId: req.params.userId });
+        res.status(200).json(orders);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching orders", error });
+    }
+});
 
 module.exports = router;
