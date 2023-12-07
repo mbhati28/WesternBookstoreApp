a65ef7391f7fd42584623f56584e7e58b33458a8
gsing352 <gsing352@uwo.ca>
2023-12-07 17:15:13 -0500

    Updated the cart page and Order page UI

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,19 +1,14 @@
 import React, { useEffect, useContext } from "react";
-import { useCart } from "../context/CartContext"; // Adjust the path based on your project structure
+import { useCart } from "../context/CartContext";
 import "./CartPage.css";
 import axios from "axios";
-import {fetchCart, removeFromCart } from "../services/api";
-import { AuthContext } from '../context/AuthContext';
-import {Link} from 'react-router-dom';
+import { fetchCart, removeFromCart } from "../services/api";
+import { AuthContext } from "../context/AuthContext";
+import { Link } from "react-router-dom";
 
 const CartPage = () => {
-  const {
-    cartItems,
-    setCartItems,
-    removeItemFromCart,
-  } = useCart();
-
-  const {authData} = useContext(AuthContext);
+  const { cartItems, setCartItems, removeItemFromCart } = useCart();
+  const { authData } = useContext(AuthContext);
 
   useEffect(() => {
     const storedCartData = localStorage.getItem("cartData");
@@ -23,22 +18,17 @@ const CartPage = () => {
   }, []);
 
   useEffect(() => {
-    // Define the function within useEffect
     const loadCartData = async () => {
-      if (!authData || !authData._id) return; // Ensure authData and its _id are available
-      const cartData = await fetchCart(authData._id);
-      setCartItems(cartData);
+      if (authData && authData._id) {
+        const cartData = await fetchCart(authData._id);
+        setCartItems(cartData);
+      }
     };
-
-    loadCartData(); // Now calling the function without arguments
+    loadCartData();
   }, [authData]);
 
-  console.log(cartItems);
-
   const handleRemoveFromCart = (itemId) => {
-
     removeItemFromCart(itemId);
-    console.log("I am inside", itemId);
     removeFromCart(authData._id, itemId);
   };
 
@@ -47,27 +37,20 @@ const CartPage = () => {
       const response = await axios.post("http://localhost:5555/api/checkout", {
         cartItems,
       });
-
-      // Redirect the user to the Stripe Checkout page
       window.location.href = response.data.url;
     } catch (error) {
       console.error("Error initiating checkout:", error);
     }
   };
 
-  if(cartItems){
-    if (cartItems.length === 0) {
-      return (
-      <div className="empty-cart-message"><h1>Your cart is empty.</h1>
-      <br></br><br></br>
-      <Link className="browse-button" to="/books">
-                    <h3>Browse Books</h3>
-                  </Link>
-                  </div>
-      );
-    }
+  // Calculate subtotal
+  const calculateSubtotal = () => {
+    return cartItems.reduce((total, item) => {
+      return total + item.saleInfo.listPrice.amount * item.quantity;
+    }, 0);
+  };
 
-
+  if (cartItems && cartItems.length > 0) {
     return (
       <div className="cart-container">
         <h1>Shopping Cart</h1>
@@ -88,22 +71,25 @@ const CartPage = () => {
             </div>
           </div>
         ))}
-        <button className="proceed-button" onClick={handlePayment}>Proceed to Checkout</button>
+        <div className="subtotal">
+          <h3>Subtotal: ${calculateSubtotal().toFixed(2)}</h3>
+        </div>
+        <button className="proceed-button" onClick={handlePayment}>
+          Proceed to Checkout
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="empty-cart-message">
+        <h1>Your cart is empty.</h1>
+        <br />
+        <Link className="browse-button" to="/books">
+          <h3>Browse Books</h3>
+        </Link>
       </div>
     );
   }
-
-else {
-  return (<div className="empty-cart-message"><h1>Your cart is empty.</h1>
-  <br></br><br></br>
-  <Link className="browse-button" to="/books">
-                <h3>Browse Books</h3>
-              </Link>
-              </div>);
-
-}
-
-
 };
 
 export default CartPage;
